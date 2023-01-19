24b5098e702c376672aa601e7092abe3da35c09a
Gazelle2010 <martin-schwerdtner@t-online.de>
2023-01-19 14:36:45 +0100

    Überlegungen - Transponierung

--- script_01.js
+++ script_01.js
@@ -3,7 +3,7 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
-let arr;
+// let arr;
 // arr = new Array();   // Konstruktor
 // arr = [];   // Literal
 // arr = [2,7,11,4]; 
@@ -66,6 +66,33 @@ function output(outputStr) {
 //     return str; 
 // }
 
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung: untereinander ---> nebeneinander
+// Helge Schneider: Anananderreihung...
+
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+//     let str = arr[0] + GAP + 
+//               arr[1] + GAP + 
+//               arr[2] + GAP + 
+//               arr[3] + GAP +
+//               arr[4] + GAP +
+//               arr[5] +
+//               PUNCT;
+
+for (let i = 0; i < arr.length; i++) {
+
+    output(arr[i]);
+}
+
+//     return str; 
+}
+
+
 /* 02a. Theorie: Schleifen (for-schleife) */
 
 // Inkrement (untere Grenze --> obere Grenze)
@@ -85,6 +112,71 @@ function output(outputStr) {
 //     output("index i: " + i);
 // }
 
-for (let i = 0; i <= 100; i++) {
-    output("index i: " + i);
+// for (let i = 0; i <= 100; i++) {
+//     output("index i: " + i);
+// }
+
+
+
+// * 02b. For-Schleife für Array-Index (Iteration)*/
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//     output("index i:" + i + " " + arr[i]); // i als Index des Arrays
+// }
+
+
+/*********   Überlegungen - Transponierung **********/
+
+
+
+/*
+
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+
+Solange die Variable existiert, bleibt dieser erhalten.
+
+hier: Aufsummierung
+
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+let a = 0; // Anfangswert
+
+// for (let i = 0; i < 10; i++) {
+//     output (i);
+//     output("in der loop vor +:" + a);
+//     a = a + 1;
+//     output("in der loop nach +:" + a);
+// }
+
+ 
+// output("nach der loop: " + a);
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+ 
+
+let str = "";
+let gap = " ";
+let addStr = "Test";
+for (let i = 0; i <= 5; i++) {
+    output("in der loop: " + str);
+    // a = a + 1;  // Numerik
+    str = str + addStr + gap // Text
 }
+output("nach der loop: " + str);
+
